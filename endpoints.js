151e14fc9e6bbcab53306accccc51615a1bc4dd1
Suraj <surajchan68@gmai.com>
2020-04-21 13:35:36 +0545

    https://swagger.io/specification/ is used for more

--- endpoints.js
+++ endpoints.js
@@ -1,21 +1,29 @@
 const initializeEndpoints = (app) => {
 
-/**
-* @swagger
-* /users:
-*   get:
-*     summary: Get all Users.
-*     description: This route get all Users.
-*     parameters:
-*        - in: query
-*          name: format
-*          type: string
-*          description: 'The format to return the response in, i.e. json.'
-*          default: json
-*     responses:
-*           '200':
-*              description: Success           
-*/
+    /**
+    * @swagger
+    * /users:
+    *   get:
+    *     summary: Get all Users.
+    *     description: This route get all Users.
+    *     parameters:
+    *        - in: query
+    *          name: format
+    *          type: string
+    *          description: 'Getting all users information.'
+    *          schema: 
+    *            type: object
+    *            description: 'get user list'
+    *     responses:
+    *           '200':
+    *              description: Success
+    *              schema:
+    *                type: object
+    *                properties:
+    *                  ip:
+    *                    type: string
+    *                    example: [123.123.100]           
+    */
     app.get('/users', (req, res) => {
         res.send('This should return all users');
     });
@@ -23,7 +31,7 @@ const initializeEndpoints = (app) => {
     /**
      * @swagger
      * 
-     * /users/:id:
+     * /users/{id}:
      *  get:
      *   description:This Should return all users
      *  responses:
