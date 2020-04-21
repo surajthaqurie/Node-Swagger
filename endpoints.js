5b729cc41bcd6364e5e84e580f728fe5b420b466
Suraj <surajchan68@gmai.com>
2020-04-21 12:35:57 +0545

    updating swagger and implements paramaters

--- endpoints.js
+++ endpoints.js
@@ -1,16 +1,22 @@
 const initializeEndpoints = (app) => {
 
-    /**
-     * @swagger
-     * 
-     * /users:
-     *  get:
-     *   description:This Should return all users
-     *  responses:
-     *         '200':
-     *            description: A successful response
-     */
-    app.get('/users/:id', (req, res) => {
+/**
+* @swagger
+* /users:
+*   get:
+*     summary: Get all Users.
+*     description: This route get all Users.
+*     parameters:
+*        - in: query
+*          name: format
+*          type: string
+*          description: 'The format to return the response in, i.e. json.'
+*          default: json
+*     responses:
+*           '200':
+*              description: Success           
+*/
+    app.get('/users', (req, res) => {
         res.send('This should return all users');
     });
 
